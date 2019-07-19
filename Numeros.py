a1c5fa5172ff804bc77ddedc97a8f95d09c13543
Public <rofocale.lucifugo.lr@gmail.com>
2019-07-19 15:54:37 +0200

    3

--- Numeros.py
+++ Numeros.py
@@ -4,3 +4,5 @@ print ("1.5 + 1 =", 1.5+1)
 edad=input("Inserta tu edad: ")
 dobleEdad = int(edad) * 2 #Hay que convertir edad, que es un string en un entero
 print("Tienes", edad,"años. Y el doble de tu edad es:", dobleEdad)
+
+print(23)
