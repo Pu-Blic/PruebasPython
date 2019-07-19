fdfaae2cdf42f741dad4b3dd59d119a673513e47
Nada <elpublic@hotmail.com>
2019-07-19 09:52:18 +0200

    Agregar archivos de proyecto.

---
+++ Datatype.py
@@ -0,0 +1,42 @@
+print("Hola")
+print(type("Hola"))
+print(23)
+print(type(23))
+
+print("Hola" + " " + "Mundo")
+
+#Número entero
+print(23) 
+print(type(23))
+#Número decimal
+print(23.5) 
+print(type(23.5))
+
+#booleanos
+print(True)
+print(type(True))
+
+#listas, arrays
+print([12, 14,16,18])
+print(type([12, 14,16,18]))
+
+print(["rojo", "verde", "azul", "verde"])
+print(type(["rojo", "verde", "azul", "verde"]))
+
+print([1, "Ana", "verde", 17.50])
+print(type([1, "Ana", "verde", 17.50]))
+
+#Tuples. es una lista que no puede modificarse
+print((10, 20, 30, 40))
+print(type((10, 20, 30, 40)))
+
+#Diccionarios. Agrupa datos de una misma entidad. Son como estructuras de datos
+print({"nombre":"Juan",
+ "apellidos":"López",
+ "Edad":"41"
+})
+print(type({"nombre":"Juan",
+ "apellidos":"López",
+ "Edad":"41"
+}))
+
